2de3658067bc46a269bad0219cd8dabf04660570
joehanzsenna <jonathanedison34@gmail.com>
2024-02-28 16:28:05 +0100

    updates

--- src/app/(workgroup)/signUp/page.jsx
+++ src/app/(workgroup)/signUp/page.jsx
@@ -26,6 +26,16 @@ export default function SignUpPage() {
     username: "",
     password: "",
   });
+  const cors = require('cors');
+  app.use(cors())
+
+  router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
